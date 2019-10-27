8abfb3e308e867fbfbc5c94eebcaf0bf52466922
howdy39 <tatsuya.howdy@gmail.com>
2019-10-27 20:15:06 +0900

    add: chapter1

---
+++ chapter1/src/test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function test(arg) {
+    var nullAble = null;
+    nullAble = 'string';
+    return arg;
+}
+exports.test = test;
