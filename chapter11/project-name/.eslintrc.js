3d90365754d3404911caeb4512c81c2ea050e795
howdy39 <tatsuya.howdy@gmail.com>
2020-01-05 23:16:50 +0900

    add files

---
+++ chapter11/project-name/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  // add your custom rules here
+  rules: {
+  }
+}
